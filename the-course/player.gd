2c7647d2bfcaa205da9b6d8a76b8aaa531ce8cb6
skooter500 <bryan.duggan@tudublin.ie>
2024-09-30 16:55:01 +0100

    week3

---
+++ the-course/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+@export var bullet_scene:PackedScene
+@export var bullet_spawn_point:Node2D
+
+const SPEED = 500.0
+const JUMP_VELOCITY = -400.0
+
+const TURN_RATE = 180
+
+func _physics_process(delta: float) -> void:
+	
+	var r = Input.get_axis("turn_left", "turn_right")
+	print(r)
+
+	var rot = deg_to_rad(r * TURN_RATE * delta)
+	rotate(rot)
+	
+	var f = Input.get_axis("move_backwards", "move_forwards")
+
+	var vel = transform.y * f * SPEED
+	print(transform.y)
+	velocity = vel
+	
+	if Input.is_action_pressed("fire"):
+		var b = bullet_scene.instantiate()
+		b.global_position = bullet_spawn_point.global_position
+		get_parent().add_child(b)
+	
+	move_and_slide()
