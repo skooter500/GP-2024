a999dd3d6d3432be7ed23dcb7a92103e1893fd84
C24453362 Jessica Keating Boylan <C24453362@myTUDublin.ie>
2024-10-14 16:11:14 +0100

    AHHHH

--- the-course/bullet.gd
+++ the-course/bullet.gd
@@ -1,23 +1,15 @@
-extends CharacterBody2D
+extends Area2D
 
-@export var speed = 20
+@export var Bullet : PackedScene
 
-@export var explosion:PackedScene
+var speed = 750
 
-	
-func _process(delta):
-	velocity = -transform.y * speed
-	var c =  move_and_collide(velocity * delta)
-	if c:
-		if c.get_collider().name.contains("block"):
-			c.get_collider().queue_free()
-			var e = explosion.instantiate()
-			get_tree().get_root().add_child(e)
-			e.global_position = self.global_position
-			e.emitting = true
-			self.queue_free()
+func _physics_process(delta):
+	position += transform.x * speed * delta
 
-
-func _on_timer_timeout():
-	self.queue_free()
-	pass # Replace with function body.
+func _on_Bullet_body_entered(body):
+	if body.is_in_group("mobs"):
+		body.queue_free()
+	queue_free()
+	if Input.is_action_just_pressed("shoot"):
+		shoot() 
