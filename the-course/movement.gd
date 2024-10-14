3e57c577648a6246b2c852ea33c1985198b924e7
mcsurvivalmode <C24385051@mytudublin.ie>
2024-10-14 15:32:43 +0100

    movement without rotation

--- the-course/movement.gd
+++ the-course/movement.gd
@@ -6,27 +6,25 @@ func _ready() -> void:
 	pass # Replace with function body.
 	
 @export var speed:float = 70
-#@export var rot_speed:float = 100
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
 	
 	
 	var s = speed 
-#var rs = rot_speed
+
 	if (Input.is_key_pressed(KEY_SHIFT)):
 		s = s * 5
-		#rs = rs * 2
+		
 		
 	if Input.is_key_pressed(KEY_UP):
 		translate(Vector2(0, -s * delta))
 	if Input.is_key_pressed(KEY_DOWN):
 		translate(Vector2(0, s * delta))
 	if Input.is_key_pressed(KEY_LEFT):
-		#rotate(-deg_to_rad(rs * delta))
 		translate(Vector2(-s * delta, 0))
 	if Input.is_key_pressed(KEY_RIGHT):
-		#rotate(deg_to_rad(rs * delta))
 		translate(Vector2(s * delta, 0))
 	pass
 		
