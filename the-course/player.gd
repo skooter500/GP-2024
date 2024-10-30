1499b55771bc3bd428d90143f58f1d564b2182f8
y2kpartytime <C24732831@myTUDublin.ie>
2024-10-30 06:16:29 +0000

    updated player controls/script and bullet and environment

--- the-course/player.gd
+++ the-course/player.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
+
 @export var bullet_scene:PackedScene
 @export var bullet_spawn_point:Node2D
+@onready var shoot_timer: Timer = $shoot_timer
 
 const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
 
 const TURN_RATE = 180
+
+var shooting = false
 
 func _physics_process(delta: float) -> void:
 	
@@ -22,11 +26,18 @@ func _physics_process(delta: float) -> void:
 	print(transform.y)
 	velocity = vel
 	
+
 	
-	if Input.is_action_pressed("fire"):
+	if Input.is_action_pressed("fire") and shooting == false:
+		shooting = true
 		var b = bullet_scene.instantiate()
 		b.global_position = bullet_spawn_point.global_position
 		b.global_rotation = bullet_spawn_point.global_rotation
 		get_parent().add_child(b)
+		shoot_timer.start()
 	
 	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	shooting = false
