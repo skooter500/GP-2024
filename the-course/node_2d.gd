bb2b12fc088c22cfa03a37eb1b3120e4f6b4f088
C24383586 Omar Aglan <C24383586@myTUDublin.ie>
2024-11-04 16:09:33 +0000

    Commit2

--- the-course/node_2d.gd
+++ the-course/node_2d.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+<<<<<<< HEAD
 
 <<<<<<< HEAD
 @export var bug_scene:PackedScene
@@ -19,9 +20,35 @@ func _ready() -> void:
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 >>>>>>> e25075f (Commit1)
+=======
+@export var speed = 10
+@export var player = $Player
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	var a:float = 10.0 / 17.0
+	var b:float
+	
+	b = a * 17.0
+	print(b)
+	
+>>>>>>> a9b956d (Commit2)
 	pass # Replace with function body.
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
+	
+	
+	if to_player.length() > 0.1:
+		DebugDraw2D.set_text("Dist to player", to_player)
+	var to_player:Vector3 = player.global_position - global_position
+	
+	DubugDraw2D.set_text("Dist to player", to_player.length())
+	
+	to_player = to_player.normalized()
+	DubugDraw2D.set_text("Normalized to player", to_player.length())
 	pass
