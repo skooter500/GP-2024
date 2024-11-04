02a3e1f46cefee38c774a54fd12edbe6ea8d37ab
MassiveLegend2345 <cdunne077@gmail.com>
2024-11-04 15:04:10 +0000

    Week 8, Sprite changes
    Tried to get collision end game

--- the-course/rigid_body_2d.gd
+++ the-course/rigid_body_2d.gd
@@ -7,6 +7,17 @@ var thrust = Vector2.ZERO
 var rotation_dir = 0
 var esc = KEY_ESCAPE
 
+func _on_CollisionShape2D_area_entered(area):
+	if area.get_parent().name.begins_with("shidstain"):
+		get_tree().quit()
+	pass
+
+func _on_CollisionShape2D_body_entered(body):
+	if body.name.begins_with("shidstain"):
+		print("collided with", body.get_parent().get_name())
+		get_tree().quit()
+	pass
+	
 func get_input():
 	thrust = Vector2.ZERO
 	if Input.is_action_pressed("thrust"):
@@ -14,6 +25,7 @@ func get_input():
 	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
 	if Input.is_action_pressed("esc"):
 		get_tree().change_scene_to_file("res://Bomber Man/Menu for bomb ber.tscn")		
+
 	
 func _physics_process(_delta):
 	get_input()
