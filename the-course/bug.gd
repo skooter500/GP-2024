206c6577057cb7d767eef25af3465b1c313de00c
skooter500 <bryan.duggan@tudublin.ie>
2024-11-11 22:50:34 +0000

    bug movement

--- the-course/bug.gd
+++ the-course/bug.gd
@@ -12,23 +12,23 @@ signal bug_hit
 func _ready():
 	pass # Replace with function body.
 
+var speed = 50
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	global_position = lerp(global_position, target_pos, delta)
+	global_position = global_position.move_toward(target_pos, delta * speed)
 	pass
-
 
 func _on_timer_timeout():
 	var offset = randf_range(-100, 100)
-	target_pos = position
+	target_pos = global_position
 	target_pos.x += offset
-	if target_pos.x < 20:
-		target_pos.x = 20
-	var w = get_viewport_rect().size.x
-	if target_pos.x > w - 20:
-		target_pos.x = w - 20 
-	target_pos.y += 20
+	#if target_pos.x < 20:
+		#target_pos.x = 20
+	#var w = get_viewport_rect().size.x
+	#if target_pos.x > w - 20:
+		#target_pos.x = w - 20 
+	#target_pos.y += 20
 	if randi_range(0, 2) == 0:
 		$AudioStreamPlayer2D.play()
 	
--- the-course/bug_spawner.gd
+++ the-course/bug_spawner.gd
@@ -9,6 +9,6 @@ func _on_timer_timeout():
 	var s = get_viewport().size
 	var bug = bug_scene.instantiate()
 	bug.position.x = randf_range(20, s.x - 20)
-	bug.position.y = randf_range(20, s.x - 20)
+	bug.position.y = 50
 	get_tree().root.add_child(bug)
 	pass # Replace with function body.
