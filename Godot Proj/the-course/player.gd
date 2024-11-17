2e9548ecee1e946f79b8bea42c657006656317f9
y2kpartytime <C24732831@myTUDublin.ie>
2024-11-17 23:40:55 +0000

    Speedboost
    Added speed-boost
    Changed Sprites
    
    Planning on adding flames/trail to back of ship on boost.

--- Godot Proj/the-course/player.gd
+++ Godot Proj/the-course/player.gd
@@ -4,12 +4,17 @@ extends CharacterBody2D
 @export var bullet_spawn_point:Node2D
 
 var can_shoot = true
+var can_boost = false
 
-const SPEED = 250.0
+@export var SPEED = 250.0
+var SPEEDBOOST = 700.0
 const shoot_speed = 5
 const JUMP_VELOCITY = -400.0
 
 const TURN_RATE = 180
+
+func _ready() -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
 	
@@ -18,12 +23,14 @@ func _physics_process(delta: float) -> void:
 
 	var rot = deg_to_rad(r * TURN_RATE * delta)
 	rotate(rot)
-	
-	var f = Input.get_axis("move_backwards", "move_forwards")
 
+	var f = Input.get_axis("move_backwards", "move_forwards")
 	var vel = transform.y * f * SPEED
-	print(transform.y)
 	velocity = vel
+	
+	if Input.is_action_pressed("boost"):
+		var new_vel = transform.y * f * SPEEDBOOST
+		velocity = new_vel
 	
 	if Input.is_action_pressed("fire") and can_shoot:
 		var b = bullet_scene.instantiate()
@@ -38,4 +45,4 @@ func _physics_process(delta: float) -> void:
 
 func _on_timer_timeout():
 	can_shoot = true
-	pass # Replace with function body.
+	pass
