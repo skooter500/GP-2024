c7e7f53f6acd4353113086eeb0118ba45b6e4193
AmazenYT <frankiecahill90@gmail.com>
2024-11-25 12:30:05 +0000

    test

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -1,9 +1,16 @@
 extends Node2D
+var i = 0
+
 
 func _draw() -> void:
+	i += 1
+	print ("draw called")
 	var r = get_viewport_rect()
 	r.size.x = r.size.x / 2
 	draw_rect(Rect2(r), Color.RED, true, 10, true)
+	var p = get_viewport().get_mouse_position()
+	if p.x  < 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
