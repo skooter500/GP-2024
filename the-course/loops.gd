f95339b96be042fe8333bd26103859bdd6cc90dd
AmazenYT <frankiecahill90@gmail.com>
2025-01-30 22:06:49 +0000

    drawing with loops

--- the-course/loops.gd
+++ the-course/loops.gd
@@ -14,3 +14,19 @@ func _ready() -> void:
 			row += "(" + str(x) + "," + str(y) + ")"
 		print(row)
 		
+func _draw() -> void:
+	var screen_width = get_viewport_rect().size.x
+	var screen_height = get_viewport_rect().size.y
+	var num_lines = 10
+	var spacing_x = screen_width / (num_lines - 1)  # Space for vertical lines
+	var spacing_y = screen_height / (num_lines - 1)  # Space for horizontal lines
+
+	# Draw horizontal lines in RED
+	for i in range(num_lines):
+		var y = i * spacing_y
+		draw_line(Vector2(0, y), Vector2(screen_width, y), Color.RED, 2)
+
+	# Draw vertical lines in BLUE
+	for i in range(num_lines):
+		var x = i * spacing_x
+		draw_line(Vector2(x, 0), Vector2(x, screen_height), Color.BLUE, 2)
