83104c0ba76e487ddac4d10c40fa7bab5bea8d05
C24383586 <C24383586@myTUDublin.ie>
2025-01-30 12:29:27 +0000

    jfbvlie

--- the-course/if_statement.gd
+++ the-course/if_statement.gd
@@ -31,7 +31,43 @@ func _draw() -> void:
 		draw_rect(Rect2(h, 0, h, r.size.y), Color.YELLOW, true, 10, true)
 		draw_rect(Rect2(h, w, h, r.size.y), Color.GREEN, true, 10, true)
 		draw_rect(Rect2(0, w, h, r.size.y), Color.RED, true, 10, true)
-	# ==p
+	## ==p
+	## <
+	## >
+	## <=
+	## >=
+	## && and
+	## || or
+	## not
+=======
+<<<<<<< HEAD
+var i = 0
+var thera 
+
+func _draw() -> void:
+	
+	
+	
+	print("draw called: " + str(i))
+	var r = get_viewport_rect()
+	var hw = r.size.x / 2
+	var hh = r.size.y / 2
+	draw_rect(Rect2(r), Color.RED, true, 10, true)
+	
+	var p = get_viewport().get_mouse_position()
+	print(p)
+	draw_rect(Rect2(0, 0, r.size.x, r.size.y), Color.BLUE, true, 10, true)
+	if p.x < hw and p.y < hh:
+		draw_rect(Rect2(0, 0, hw, hh), Color.RED, true, 10, true)
+	elif p.x < hw and p.y > hh:
+		draw_rect(Rect2(0, hh, hw, hh), Color.RED, true, 10, true)
+	elif p.x > hw and p.y < hh:
+		draw_rect(Rect2(hw, 0, hw, hh), Color.RED, true, 10, true)
+	else:
+		draw_rect(Rect2(hw, hh, hw, hh), Color.RED, true, 10, true)
+	
+
+	#    ==
 	# <
 	# >
 	# <=
