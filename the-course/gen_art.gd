e9ab08091eb705bb7fa5b0bcb29d12722b38e603
amy <C24399391@myTUDublin.ie>
2025-02-06 13:40:28 +0000

    spiral work

--- the-course/gen_art.gd
+++ the-course/gen_art.gd
@@ -6,20 +6,19 @@ var count:int
 
 func _draw() -> void:
 	var s = float(count)
-	for i in range(s):
-		draw_line(Vector2(i * 100, 0), Vector2(i * 100, 500), Color.DARK_BLUE, 10)
+	#for i in range(s):
+		#draw_line(Vector2(i * 100, 0), Vector2(i * 100, 500), Color.DARK_BLUE, 10)
 	
 	var theta_inc= 1
 	var px = 0
 	var py = 0
-	var radius = 0
 	var r = 0
 	for i in range(s):
 		var theta= theta_inc * i
-		var x = sin(theta) * radius
-		var y= cos(theta) * radius
-		draw_line(Vector2(px,py), Vector2(x, y), Color.DARK_GREEN, 10)
-		draw_circle(Vector2(x,y), 10, Color.HOT_PINK, false)
+		var x = sin(theta) * r
+		var y= cos(theta) * r
+		draw_line(Vector2(px,py), Vector2(x, y), Color.MEDIUM_PURPLE, 10)
+		#draw_circle(Vector2(x,y), 10, Color.HOT_PINK, false)
 		px = x
 		py = y
 		r = r + 10
