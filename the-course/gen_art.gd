c7b472bbb308b9146bf6d98d202ad8cb675223df
amy <C24399391@myTUDublin.ie>
2025-02-06 13:00:11 +0000

    not drawing spiral

--- the-course/gen_art.gd
+++ the-course/gen_art.gd
@@ -5,16 +5,25 @@ var count:int
 
 
 func _draw() -> void:
-	var s:float = $num_slider.value
+	var s = float(count)
 	for i in range(s):
 		draw_line(Vector2(i * 100, 0), Vector2(i * 100, 500), Color.DARK_BLUE, 10)
 	
-	var theta_inc= PI*2.0/s
+	var theta_inc= 1
+	var px = 0
+	var py = 0
+	var radius = 0
+	var r = 0
 	for i in range(s):
 		var theta= theta_inc * i
 		var x = sin(theta) * radius
 		var y= cos(theta) * radius
-		draw_circle(Vector2(x,y), radius, Color.HOT_PINK, false)
+		draw_line(Vector2(px,py), Vector2(x, y), Color.DARK_GREEN, 10)
+		draw_circle(Vector2(x,y), 10, Color.HOT_PINK, false)
+		px = x
+		py = y
+		r = r + 10
+		
 	pass
 	
 func _process(delta: float) -> void:
@@ -26,5 +35,5 @@ func _on_num_slider_value_changed(value: float) -> void:
 
 
 func _on_rad_slider_value_changed(value: float) -> void:
-	count = value
+	radius = value
 	pass # Replace with function body.
