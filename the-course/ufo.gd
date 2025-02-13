d308bf2a6647f5718ac55f9bb33651003bc5b7bf
skooter500 <skooter500@gmail.com>
2025-02-13 00:23:11 +0000

    brilliant

--- the-course/ufo.gd
+++ the-course/ufo.gd
@@ -17,6 +17,7 @@ func _ready() -> void:
 	scale = Vector2.ZERO
 	var tween = create_tween().set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_IN_OUT)
 	tween.tween_property(self, "scale", Vector2.ONE, 3)
+	$Synth.pitch_scale = randf_range(0.7, 1.3)
 	randomise_timer()
 	
 func randomise_timer():
