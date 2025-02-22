16c8d62d62ff55eee6ad1d20c2ca86f0a500492c
amy <C24399391@myTUDublin.ie>
2025-02-22 16:39:26 +0000

    worksheet
    tweening

--- the-course/enemy2.gd
+++ the-course/enemy2.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+var tween: Tween
+
+func _ready():
+	tween = create_tween()
+	tween.set_loops(0)  # Loop indefinitely
+
+	# Square movement pattern
+	tween.tween_property(self, "position", position + Vector2(200, 0), 1.0)  # Right
+	tween.tween_property(self, "position", position + Vector2(0, 200), 1.0)  # Down
+	tween.tween_property(self, "position", position + Vector2(-200, 0), 1.0)  # Left
+	tween.tween_property(self, "position", position + Vector2(0, -200), 1.0)  # Up
+
 func _draw():
 	# Draw a circle for the enemy's body
 	draw_circle(Vector2(0, 0), 20, Color(1, 0, 0))  # Red circle
