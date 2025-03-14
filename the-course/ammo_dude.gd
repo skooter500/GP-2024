751488453a7c42a866387c8010bf83fe0c5a42fc
skooter500 <skooter500@gmail.com>
2025-03-14 09:03:53 +0000

    fps

--- the-course/ammo_dude.gd
+++ the-course/ammo_dude.gd
@@ -3,7 +3,7 @@ extends Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass # Replace with function body.5
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
